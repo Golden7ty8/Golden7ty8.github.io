91fee4ba5cf7f43b927c16c140a4a4f0a6bf64c1
Golden7ty8 <alastairmay4@gmail.com>
2024-05-31 14:57:33 -0300

    Tutorial Setup Complete.

--- vite-project/main.js
+++ vite-project/main.js
@@ -42,6 +42,87 @@ scene.add(lightHelper, gridHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+function addStar() {
+
+    const geometry = new THREE.SphereGeometry(0.25);
+    const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+    const star = new THREE.Mesh(geometry, material);
+
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+    star.position.set(x, y, z);
+    scene.add(star)
+
+}
+
+Array(200).fill().forEach(addStar)
+
+
+const spaceTexture = new THREE.TextureLoader().load('Alastair Child F Icon 8.png');
+scene.background = spaceTexture;
+
+
+
+
+
+
+
+
+
+//Avatar
+
+const cubeTexture = new THREE.TextureLoader().load('Alastair Child F Icon 8.png');
+
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(3, 3, 3),
+    new THREE.MeshBasicMaterial({map: cubeTexture})
+);
+
+scene.add(cube);
+
+// Moon
+
+
+const moonTexture = new THREE.TextureLoader().load('Alastair Child F Icon 8.png');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+const moon = new THREE.Mesh(
+    new THREE.SphereGeometry(3, 32, 32),
+    new THREE.MeshStandardMaterial({
+        map: moonTexture,
+        normalMap: normalTexture
+    })
+);
+
+scene.add(moon)
+
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+
+
+function moveCamera() {
+
+    const t = document.body.getBoundingClientRect().top;
+    moon.rotation.x += 0.05;
+    moon.rotation.y += 0.075;
+    moon.rotation.z += 0.05;
+
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
+
+    camera.position.z = t * -0.01;
+    camera.position.x = t * -0.0002;
+    camera.position.y = t * -0.0002;
+
+}
+
+document.body.onscroll = moveCamera
+
+
+
+
 function animate() {
 
     requestAnimationFrame(animate);
